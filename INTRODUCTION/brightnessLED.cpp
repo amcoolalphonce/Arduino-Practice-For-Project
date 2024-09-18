fb563bf6daadecf64ed52e7a3928f006581741be
amcool <82672024+amcoolalphonce@users.noreply.github.com>
2024-09-18 20:21:47 +0300

    Function to increase and reduce brightness

--- INTRODUCTION/brightnessLED.cpp
+++ INTRODUCTION/brightnessLED.cpp
@@ -1,7 +1,22 @@
 int brightness = 0;
+
 // using PWM analogue pins ~
 
 void()
 {
   pinMode(9, OUTPUT);
+}
+
+vooid loop()
+{
+  for (brightness = 0 , brightness <= 255, brightness +=5)
+    {
+      analogWrite(9, brightness);
+      delay(20); //wait 20 milliseconds
+    }
+  for (brightness = 255, brightness > 0, brightness -= 5)
+    {
+      analogWrite(9, brightness);
+      delay(20);
+    }
 }

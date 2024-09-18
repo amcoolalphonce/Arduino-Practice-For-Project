e3394236749dca899ff805cf461e98b775bb87d8
amcool <82672024+amcoolalphonce@users.noreply.github.com>
2024-09-18 19:59:27 +0300

    function to switch o n and off the LED

--- INTRODUCTION/blinkingLED.cpp
+++ INTRODUCTION/blinkingLED.cpp
@@ -5,3 +5,11 @@ void setup()
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);  // set LED to off
 }
+
+void loop() // this function lights on and off after 4 seconds
+{
+  digitalWrite(ledPin, HIGH);
+  delay(4000); // delay 4 seconds
+  digitalWrite(ledPin, LOW);
+  delay(4000);
+}

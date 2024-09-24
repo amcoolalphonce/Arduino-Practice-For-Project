67ece91c016ebce21c179109576a170a91146d18
amcool <82672024+amcoolalphonce@users.noreply.github.com>
2024-09-24 16:42:01 +0300

    Bithday tone using buzzers

--- EXECUTED PROJECTS/happyBIRTHDAYbuzzers.cpp
+++ EXECUTED PROJECTS/happyBIRTHDAYbuzzers.cpp
@@ -1,1 +1,43 @@
+#define NOTE_C4  262
+#define NOTE_D4  294
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_G4  392
+#define NOTE_A4  440
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_D5  587
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_G5  784
 
+int melody[] = {
+  NOTE_G4, NOTE_G4, NOTE_A4, NOTE_G4, NOTE_C5, NOTE_B4, // "Happy Birthday to you"
+  NOTE_G4, NOTE_G4, NOTE_A4, NOTE_G4, NOTE_D5, NOTE_C5, // "Happy Birthday to you"
+  NOTE_G4, NOTE_G4, NOTE_G5, NOTE_E5, NOTE_C5, NOTE_B4, NOTE_A4, // "Happy Birthday dear [name]"
+  NOTE_F5, NOTE_F5, NOTE_E5, NOTE_C5, NOTE_D5, NOTE_C5 // "Happy Birthday to you"
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.: The higer the number the faster, the lower the longer
+int noteDurations[] = {
+  8, 8, 4, 4, 4, 2,
+  8, 8, 4, 4, 4, 2,
+  8, 8, 4, 4, 4, 4, 2,
+  8, 8, 4, 4, 4, 2
+};
+
+void setup() {
+  for (int thisNote = 0; thisNote < sizeof(melody)/sizeof(melody[0]); thisNote++) {
+
+    // to calculate the note duration, take one second
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(8, melody[thisNote], noteDuration);
+    int pauseBetweenNotes = noteDuration * 2.0;
+    delay(pauseBetweenNotes);  
+    noTone(8);
+  }
+}
+
+void loop() {
+  // no need to repeat the melody.
+}

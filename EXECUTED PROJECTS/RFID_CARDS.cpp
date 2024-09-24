39091e540cb0444f94b7d2278deab77544fc1d30
amcool <82672024+amcoolalphonce@users.noreply.github.com>
2024-09-24 15:15:48 +0300

    Looping functions when the cards are read

--- EXECUTED PROJECTS/RFID_CARDS.cpp
+++ EXECUTED PROJECTS/RFID_CARDS.cpp
@@ -16,3 +16,15 @@ void setup() {
 	Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
 }
 
+void loop() {
+	if ( ! mfrc522.PICC_IsNewCardPresent()) {
+		return;
+	}
+	// Select one of the cards
+	if ( ! mfrc522.PICC_ReadCardSerial()) {
+		return;
+	}
+
+	// Dump debug info about the card; PICC_HaltA() is automatically called
+	mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+}

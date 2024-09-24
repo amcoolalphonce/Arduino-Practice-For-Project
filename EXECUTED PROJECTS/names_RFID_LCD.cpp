b5e11f6fc63fd595c0bfe38f0de0320e4df018f0
amcool <82672024+amcoolalphonce@users.noreply.github.com>
2024-09-24 15:26:46 +0300

    Initialization and creating instances

--- EXECUTED PROJECTS/names_RFID_LCD.cpp
+++ EXECUTED PROJECTS/names_RFID_LCD.cpp
@@ -2,3 +2,12 @@
 #include <MFRC22.h>
 #include <WIRE.h>
 #include <LiquidCrytsal_i\I2C.h>
+
+
+// I used the I2C LCD
+LiquidCrystal_I2C lcd(0x27, 16, 2;  
+
+#define RST_PIN   9
+#define SS_PIN   10
+
+MFRC22 mfrc22(SS_PIN, RST_PIN);

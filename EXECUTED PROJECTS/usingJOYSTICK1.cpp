73e633154f2c3cdc071050645c97d142bf457cc7
amcool <82672024+amcoolalphonce@users.noreply.github.com>
2024-09-25 21:44:08 +0300

    Update usingJOYSTICK1.cpp

--- EXECUTED PROJECTS/usingJOYSTICK1.cpp
+++ EXECUTED PROJECTS/usingJOYSTICK1.cpp
@@ -3,3 +3,12 @@ const int VRxPin = A0;  // X-axis o A0
 const int VRyPin = A1;  // Y-axis to A1
 const int SWPin = 2;    // digital
 
+int xValue = 0;
+int yValue = 0;
+int buttonState = 0; 
+
+void setup() {
+  Serial.begin(9600);
+  // button pin as input with a pull-up resistor
+  pinMode(SWPin, INPUT_PULLUP);
+}
